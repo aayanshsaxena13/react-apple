070db56df685eada64a67a8811d36bce53dcd1f3
aayanshsaxena13 <aayansh.saxena13@gmail.com>
2026-05-14 17:02:46 +0530

    Updated Data Views.
    1. Changed data views to be more integrated.
    
    2. Input component now has font-medium weight to go with typography component.

--- DataViews.tsx
+++ DataViews.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Typography from "./Typography";
 
 type ListProps<ITEM> = { data: ITEM[], renderItem: (i: ITEM) => ReactNode };
 
@@ -6,13 +7,14 @@ export function List<ITEM>({ data, renderItem }: ListProps<ITEM>) {
   return (
     <div className="m-2">
       {data?.map((item: any, index: number) => (
-        <div key={index} className="bg-[#2e2e2e] p-0.5 text-white min-w-50 max-w-100" style={{
-          borderBottomWidth: data.length - 1 !== index ? 2 : 0,
+        <div key={index} className="bg-[#2e2e2e] p-px text-white min-w-50 max-w-150" style={{
+          borderBottomWidth: data.length - 1 !== index ? 1.6 : 0,
           borderColor: "rgb(116, 116, 116)",
           borderTopRightRadius: index === 0 ? 12 : 0,
           borderTopLeftRadius: index === 0 ? 12 : 0,
           borderBottomLeftRadius: data.length - 1 === index ? 12 : 0,
           borderBottomRightRadius: data.length - 1 === index ? 12 : 0,
+          overflow: "hidden",
         }}>
           {renderItem?.(item)}
         </div>
@@ -51,14 +53,14 @@ export function Table<T extends Record<string, any[]>>({
             key={item}
             className={`
               bg-[#2e2e2e] flex items-center justify-center border-gray-500
-              w-25 min-[420px]:w-30 
-              p-2 min-[420px]:p-2
-              ${index === 0 ? 'rounded-tl-2xl' : ''}
-              ${index === keys.length - 1 ? 'rounded-bl-2xl border-b-0' : 'border-b-2 min-[420px]:border-b-2'}
-              border-r-[1.6px] min-[420px]:border-r-2
+              min-[450px]:w-20 max-[450px]:w-30
+              p-px min-[450px]:p-px
+              ${index === 0 ? 'rounded-tl-xl' : ''}
+              ${index === keys.length - 1 ? 'rounded-bl-xl border-b-0' : 'border-b-[1.6px] min-[450px]:border-b-[1.6px]'}
+              border-r-[1.6px] min-[450px]:border-r-[1.6px]
             `}
           >
-            <p className="text-center text-white">{item}</p>
+            <Typography color="white" alignment="center" margin={2} type="paragraph">{item}</Typography>
           </div>
         ))}
       </div>
@@ -70,8 +72,8 @@ export function Table<T extends Record<string, any[]>>({
             key={index}
             className={`
               bg-[#2e2e2e] border-gray-500
-              ${index === 0 ? 'rounded-tr-2xl' : ''}
-              ${index === values.length - 1 ? 'rounded-br-2xl border-b-0' : 'border-b-2 min-[420px]:border-b-2'}
+              ${index === 0 ? 'rounded-tr-xl' : ''}
+              ${index === values.length - 1 ? 'rounded-br-xl border-b-0' : 'border-b-[1.6px] min-[450px]:border-b-[1.6px]'}
             `}
           >
             <div className="flex flex-row">
@@ -80,15 +82,13 @@ export function Table<T extends Record<string, any[]>>({
                   key={subIndex}
                   className={`
                     flex items-center justify-center overflow-hidden mx-1
-                    w-25 min-[420px]:w-30 
-                    p-2 min-[420px]:p-2
+                    min-[450px]:w-50 max-[450px]:w-150
+                    p-px min-[450px]:p-px
                     border-[#c7c7c7]/70
-                    ${subIndex !== item.length - 1 ? 'border-r-2 min-[420px]:border-r-2' : 'border-r-0'}
+                    ${subIndex !== item.length - 1 ? 'border-r-[1.6px] min-[450px]:border-r-[1.6px]' : 'border-r-0'}
                   `}
                 >
-                  <p className="text-center text-white">
-                    {String(subItem)}
-                  </p>
+                  <Typography color="white" alignment="center" margin={2} type="paragraph">{String(subItem)}</Typography>
                 </div>
               ))}
             </div>
